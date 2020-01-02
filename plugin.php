6dff8f40d0df89070ea1b8af42867415d7352249
Joonas Meriläinen <merilainenjoonas@gmail.com>
2020-01-02 16:28:40 +0200

    Use composer autoload

--- plugin.php
+++ plugin.php
@@ -7,8 +7,3 @@ Version:            0.0.1
 Author:             Frantic
 Author URI:         https://www.frantic.fi/
 */
-
-namespace Frc\WP\Assets;
-
-require_once __DIR__ . '/autoload.php';
-require_once __DIR__ . '/src/helpers.php';
